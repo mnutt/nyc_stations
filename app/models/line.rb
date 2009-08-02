ec0675438cc62cf6d2454eba4c22e2d693a00c34
Michael Nutt <michael@Link.local>
2009-08-02 02:03:11 -0400

    Sort the stops correctly

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,4 +1,4 @@
 class Line < ActiveRecord::Base
-  has_many :stops, :order => "position ASC"
-  has_many :stations, :through => :stops, :uniq => true
+  has_many :stops, :order => "position ASC", :conditions => "position IS NOT NULL"
+  has_many :stations, :through => :stops, :uniq => true, :conditions => "stops.position IS NOT NULL", :order => "stops.position ASC"
 end
